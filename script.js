86ec98601beff899629dab3e880a7ad261093a30
T-Heier <trentheier@gmail.com>
2021-03-24 20:54:05 -0400

    Set variables for symbols, numbers, and letters and made them into array. Made a function to prompt user to imput length of password and set an if condition that if the imput is less than 8 or more than 128 and will alert the user of the issue. Added confirm messages for variables uppercase symbols numbers and lowercase.

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+var specialSymbols = ("!#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
+var characterString = ("abcdefghijklmnopqrstuvwxyz")
+var numberString = ("1234567890")
+
+var numberArray = numberString.split("")
+var symbolArray = specialSymbols.split("")
+var characterArray = characterString.split("")
+
+console.log(symbolArray)
+console.log(numberArray)
+console.log(characterArray)
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -12,3 +26,28 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+
+  var lengthPassword = prompt("Enter length of password");
+
+  if (lengthPassword < 8 || lengthPassword > 128)  {
+
+    return alert("Password must have more than 8 characters or less than 129 characters")
+  }
+
+  var passwordNumbers = confirm("Do you want to include numbers?")
+  // if passwordNumbers = true, then add numbers to generated password
+  var passowrdUppercase = confirm("Do you want to include uppercase letters?")
+  var passowrdLowercase = confirm("Do you want to include lowercase letters?")
+  var passwordSymbols = confirm("Do you want to include special characters?")
+
+  console.log(passwordNumbers);
+
+
+  console.log(lengthPassword);
+
+
+
+
+}
